e71a157ceea50757dcdb7887690bade10e774271
PavloB <pavlo.bilousov@yahoo.com>
2025-12-29 20:57:17 +0200

    Add timer to site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// Lesson55
-// Creating tabs in a new project
-//
+// Lesson60-61
+// Create a countdown timer on a website
+// Processing a Past Date
 
 "use strict";
 
@@ -12,9 +12,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function hideTabContent() {
         tabContent.forEach(item => {
-
-            //Виконаємо заміну стилей на класи
-            //item.style.display = 'none';
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
         });
@@ -22,10 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
             item.classList.remove('tabheader__item_active');
         });
     };
-    // Значення за замовчеванням tab = 0 в аргументі функції ES6
     function showTabContent(tab = 0) {
-        //Виконаємо заміну стилей на класи
-        // tabContent[tab].style.display = 'block';
         tabContent[tab].classList.add('show', 'fade');
         tabContent[tab].classList.remove('hide');
         tabs[tab].classList.add('tabheader__item_active');
@@ -45,4 +39,75 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         };
     });
+
+    //Lesson60-61
+    //TIMER
+    const deadLine = '2026-01-01';
+
+    function getTimeRemaning(endTime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+
+        //Якщо дата акції вже пройшла, то відображаємо 00
+        if (t <= 0) {
+            const timer = document.querySelector('.timer');
+            timer.querySelector('#days').innerHTML = getZero(0);
+            hours = timer.querySelector('#hours').innerHTML = getZero(0);
+            minutes = timer.querySelector('#minutes').innerHTML = getZero(0);
+            seconds = timer.querySelector('#seconds').innerHTML = getZero(0);
+            //Інакше вираховуємо дату
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+                minutes = Math.floor((t / (1000 / 60)) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        };
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        };
+    };
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        //Для запуску таймеру зразу після завантиаження, не чекаючи 1000 мілісекунд
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        };
+    };
+
+    //Якщо дата акції не пройшла відображаємо запускаємо таймер
+    if (Date.parse(deadLine) - Date.parse(new Date()) > 0) {
+        setClock('.timer', deadLine);
+    } else {
+
+        getTimeRemaning(deadLine);
+    };
 });
